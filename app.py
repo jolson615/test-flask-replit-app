797c0908a10d959e88efce808e441fc694340300
Jeff <jolson615@gmail.com>
2022-02-24 20:29:08 +0000

    We attempted to restructure the entire app, adding replit compatibility for the whole thing!

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
-
+from flask import render_template
+from flask import request
+from flask import redirect
 
 # -- Initialization section --
 app = Flask(__name__)
@@ -13,4 +13,16 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route('/sendBreakfast', methods=['GET', 'POST'])
+def results():
+    if request.method == 'GET':
+      return redirect("/")
+    else:
+      return request.form
+
+@app.route('/secret')
+def secret():
+    return "You found the secret route"
+
